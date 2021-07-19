8c6c8bd85fc0b45561bfa147dd29c75832e2d290
Alex Bri <alverill@gmail.com>
2021-07-19 15:17:46 +0300

    Added simple navigation

--- navigation/PageNavigation.js
+++ navigation/PageNavigation.js
@@ -1,19 +1,36 @@
 import React from 'react'
-import {createAppContainer} from "react-navigation";
-import {createStackNavigator} from "react-navigation-stack";
-
+import {createAppContainer, StackViewTransitionConfigs} from "react-navigation";
+// import {createStackNavigator} from "react-navigation-stack";
+import {createStackNavigator, CardStyleInterpolators} from '@react-navigation/stack';
 import DetailScreen from "../screens/DetailScreen";
 import SearchScreen from "../screens/SearchScreen";
+import {NavigationContainer} from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+const addConfig = {
+    // gestureResponseDistance: {
+    //     horizontal: '100%',
+    // },
+    gestureDirection: 'horizontal',
+    gestureEnabled: true,
+    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+    detachPreviousScreen: false,
+}
+
+const MainNavigation = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="SearchScreen"
+                headerMode="hidden"
+                screenOptions={() => addConfig}>
+                <Stack.Screen name="Search" component={SearchScreen}/>
+                <Stack.Screen name="Detail" component={DetailScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
 
 
-const MainNavigation = createStackNavigator({
-        Search: SearchScreen,
-        Detail: DetailScreen,
-    }, {
-        defaultNavigationOptions: { headerShown: false}
-    }
-);
-
-
-export default createAppContainer(MainNavigation)
+export default MainNavigation
 
