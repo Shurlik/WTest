508266ceb11150838b6ad4ea787b460fabe228ab
Alex Bri <alverill@gmail.com>
2021-07-19 11:04:27 +0300

    first commit

---
+++ screens/DetailScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { ScaledSheet } from 'react-native-size-matters';
+
+const DetailScreen = () => {
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    )
+}
+
+export default DetailScreen
+
+const styles = ScaledSheet.create({})
---
+++ screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { ScaledSheet } from 'react-native-size-matters';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}
+
+export default SearchScreen
+
+const styles = ScaledSheet.create({})
